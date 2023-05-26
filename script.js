27ca1964cd76c0cc19c93efbf86d132db7b362a5
aprendisdeprogramas <133019961+aprendisdeprogramas@users.noreply.github.com>
2023-05-25 22:12:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,7 @@
 $(document).ready(function(){
 $(".image").ripples({
     resolution: 512,
-    dropRadius: 10,
     perturbance: 0.04,
-    interactive: false,
-    crossOrigin: "",
-    imageUrl: null,
+   
   })
 });
