c69c26f1026b758a2fbf5829b1d6f53d98cd18d7
aprendisdeprogramas <133019961+aprendisdeprogramas@users.noreply.github.com>
2023-05-25 22:15:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-
-$(document).ready(function(){
 $(".image").ripples({
     resolution: 512,
+    dropRadius: 10,
     perturbance: 0.04,
-   
-  })
+    interactive: true, // Habilitar la interacción
+    crossOrigin: "",
+    imageUrl: null,
 });
+
+// Agregar evento touchstart para dispositivos móviles
+$(".image").on("touchstart", function(event) {
+    $(this).ripples("drop", event.originalEvent.touches[0].pageX, event.originalEvent.touches[0].pageY, Math.random() * 3 + 2);
+});
+
