e217835a8ac1ae67df4b1cb9a6e07345c4a600ed
aprendisdeprogramas <133019961+aprendisdeprogramas@users.noreply.github.com>
2023-05-25 22:30:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
-  $(".image").ripples({
+$(".image").ripples({
     resolution: 512,
     dropRadius: 10,
     perturbance: 0.04,
-    interactive: true,
+    interactive: true, // Habilitar la interacción
     crossOrigin: "",
-    imageUrl: null
-  });
+    imageUrl: null,
+});
 
-  $(".image").on("touchstart", function(event) {
+// Agregar evento touchstart para dispositivos móviles
+$(".image").on("touchstart", function(event) {
     $(this).ripples("drop", event.originalEvent.touches[0].pageX, event.originalEvent.touches[0].pageY, Math.random() * 3 + 2);
-  });
 });
